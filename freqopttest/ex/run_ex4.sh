fcfd1f3d8f9da80030ebbab004d802f0925d818a
Wittawat Jitkrittum <wittawatj@gmail.com>
2016-01-25 20:23:29 +0000

    added a short description of the NIPS text experiment

--- freqopttest/ex/run_ex4.sh
+++ freqopttest/ex/run_ex4.sh
@@ -3,4 +3,5 @@
 python ex4_text.py bayes_neuro_d300_rnoun
 python ex4_text.py bayes_neuro_d2000_rnoun
 python ex4_text.py bayes_neuro_d800_rverb
-
+python ex4_text.py deep_learning_d1000_rnoun
+python ex4_text.py bayes_deep_d1000_rnoun
