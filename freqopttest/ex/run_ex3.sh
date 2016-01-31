3cf3ffdea1009700f1f4d7e536c9f2798c1fca7f
Wittawat Jitkrittum <wittawatj@gmail.com>
2016-01-31 15:41:22 +0000

    icml: provide all selected important terms in the NIPS experiment

--- freqopttest/ex/run_ex3.sh
+++ freqopttest/ex/run_ex3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
+python ex3_vary_nlocs.py sg_d50
 python ex3_vary_nlocs.py SSBlobs
 python ex3_vary_nlocs.py gmd_d100 
 python ex3_vary_nlocs.py gvd_d50
-python ex3_vary_nlocs.py gvd_d100
-python ex3_vary_nlocs.py sg_d50
+#python ex3_vary_nlocs.py gvd_d100
