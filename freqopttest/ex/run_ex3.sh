742fe8c389629c93399e981c8063d2d4f0444887
Wittawat Jitkrittum <wittawatj@gmail.com>
2016-11-13 14:24:35 +0000

    create gh-pages branch

--- freqopttest/ex/run_ex3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-python ex3_vary_nlocs.py gmd_d2 
-#python ex3_vary_nlocs.py sg_d50
-#python ex3_vary_nlocs.py SSBlobs
-#python ex3_vary_nlocs.py gmd_d100 
-#python ex3_vary_nlocs.py gvd_d50
-#python ex3_vary_nlocs.py gvd_d100
--- freqopttest/ex/run_ex5.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-#python ex5_face.py S48_HANESU_AFANDI
-python ex5_face.py crop48_h0
-python ex5_face.py crop48_HANESU_AFANDI
-
